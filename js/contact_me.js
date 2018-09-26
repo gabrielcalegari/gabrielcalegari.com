0d14b1c7c44b8f4e1a370a97bf4f6e709f4fb5cd
gabrielcalegari <gabrielcalegari@gabrielcalegari.com>
2018-09-26 17:43:13 -0300

    Corrigindo form

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,7 +10,6 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -18,12 +17,11 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "./mail/contact_me.php",
+                url: "https://formspree.io/site@gabrielcalegari.com",
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
-                    email: email,
+                    _replyto: email,
                     message: message
                 },
                 cache: false,
